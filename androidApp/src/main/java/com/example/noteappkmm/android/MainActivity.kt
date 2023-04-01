47cdd2a8048b46029861d9c8d3f53a45ec365097
Ashutosh ojha <Ashutosh.Ojha2@commscope.com>
2023-04-01 21:34:30 +0530

    di added

--- androidApp/src/main/java/com/example/noteappkmm/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/noteappkmm/android/MainActivity.kt
@@ -9,18 +9,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.noteappkmm.Greeting
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    GreetingView(Greeting().greet())
-                }
+             NoteListScreen()
             }
         }
     }
